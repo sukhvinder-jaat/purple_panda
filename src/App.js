627fc004028e4f1f71312db3cef0526ddb323318
sukhvinder-jaat <monudhaka829@gmail.com>
2023-07-07 16:21:08 +0530

    bootstap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+     
+    </div>
+  );
 }
 
 export default App;
