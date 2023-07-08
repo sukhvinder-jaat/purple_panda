a7b2e6ef1f829c283b3c81ce86e97b3eeee8bb8a
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-07-08 09:44:31 +0530

    ourrange

---
+++ src/components/OurRange.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const OurRange = () => {
+  return (
+    <>
+          <section className="pt-5">
+              <div className="container_1140 pt-5">
+                  
+              </div>
+              
+      </section>
+    </>
+  );
+};
+
+export default OurRange;
