22e74d96db73e6185c92223abb86595801964104
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-07-08 10:08:06 +0530

    colorpurple

--- src/components/OurRange.jsx
+++ src/components/OurRange.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 
 const OurRange = () => {
   return (
     <>
-          <section className="pt-5">
-              <div className="container_1140 pt-5">
-                  
+      <section className="pt-5">
+        <div className="container_1140 pt-5">
+          <Row className="pt-3">
+            <Col lg={12}>
+              <div className="our_range_box">
+                <p>
+                  Our <span>Range</span>
+                </p>
               </div>
-              
+            </Col>
+          </Row>
+        </div>
       </section>
+      
     </>
   );
 };
