13bf8b2efa4c411cbbb199ff2e63b78629a98103
sukhvinder-jaat <monudhaka829@gmail.com>
2023-07-08 10:26:38 +0530

    nav

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import nav_btn from "../assets/img/svg/nav_panda.svg";
 import shopping from "../assets/img/svg/nav_shoping.svg";
-// import search from "../assets/images/svg/search_icon.svg";
+import search from "../assets/img/svg/nav_search.svg";
+import gift from "../assets/img/svg/top_gift.svg";
 import { ImCross } from "react-icons/im";
 import { AiOutlineAlignRight } from "react-icons/ai";
 import { Container } from "react-bootstrap";
@@ -18,9 +19,21 @@ const MyNav = () => {
   return (
     <>
       <div className="bg_backblack">
+        <div className="bg_nav_top py-3 d-flex align-items-center justify-content-center">
+          <span className=" d-flex align-items-center ">
+            <img src={gift} alt="gift" className="w-100" />
+          </span>
+          <p className=" mb-0 text-white fs_md fw_800 ff_Montserrat ms-3 pt-3">
+            Black Friday{" "}
+            <span className="fw_400">
+              savings are here ,save up to 10% off on coupon code
+            </span>
+          </p>
+          <p className=" ff_Montserrat fs_md fw_800 mb-0 pt-2 clr_purple">Shop Now</p>
+        </div>
         <Container className="container_1140">
           {/* btn */}
-          <div className=" d-flex justify-content-between d-xl-none align-items-center px-md-4 py-3">
+          <div className=" d-flex justify-content-between d-lg-none align-items-center px-md-4 py-3">
             <span>
               <a href="#">
                 <img
@@ -42,8 +55,8 @@ const MyNav = () => {
             <div className="py-4 vh_100_1920 position-relative">
               <div className="mx-md-5 mx-sm-3 mx-1">
                 <div className="nav_container">
-                  <div className=" d-flex align-items-center flex-xl-row flex-column justify-content-center justify-content-xl-between  vh_100_992">
-                    <span className=" d-xl-block d-none me-3">
+                  <div className=" d-flex align-items-center flex-lg-row flex-column justify-content-center justify-content-lg-between  vh_100_992">
+                    <span className=" d-lg-block d-none me-3">
                       <img
                         src={nav_btn}
                         alt="nav_logo"
@@ -52,11 +65,11 @@ const MyNav = () => {
                     </span>
                     {/* option */}
                     <div className=" d-flex align-items-center">
-                      <ul className="d-flex align-items-center mb-xl-0 flex-xl-row flex-column ps-0 gap-xl-0 gap-2 mb-4">
+                      <ul className="d-flex align-items-center mb-lg-0 flex-lg-row flex-column ps-0 gap-lg-0 gap-2 mb-4">
                         <li>
                           <a
                             href="#"
-                            className="me-xl-4 opacity_06 ff_way clr_white fw_400 fs_sm"
+                            className="me-lg-4 ff_Montserrat clr_white fw_400 fs_md hover_bold hover_line position-relative"
                             onClick={clickshow}
                           >
                             HOME
@@ -65,7 +78,7 @@ const MyNav = () => {
                         <li>
                           <a
                             href="#"
-                            className="me-xl-4 opacity_06 ff_way clr_white fw_400 fs_sm"
+                            className="me-lg-4 ff_Montserrat clr_white fw_400 fs_md hover_bold hover_line position-relative"
                             onClick={clickshow}
                           >
                             SHOP
@@ -74,7 +87,7 @@ const MyNav = () => {
                         <li>
                           <a
                             href="#"
-                            className="me-xl-4 opacity_06 ff_way clr_white fw_400 fs_sm"
+                            className="me-lg-4 ff_Montserrat clr_white fw_400 fs_md hover_bold hover_line position-relative"
                             onClick={clickshow}
                           >
                             ABOUT
@@ -83,7 +96,7 @@ const MyNav = () => {
                         <li>
                           <a
                             href="#"
-                            className="me-xl-4 opacity_06 ff_way clr_white fw_400 fs_sm"
+                            className="me-lg-4 ff_Montserrat clr_white fw_400 fs_md hover_bold hover_line position-relative"
                             onClick={clickshow}
                           >
                             CONTACT
@@ -92,30 +105,60 @@ const MyNav = () => {
                         <li>
                           <a
                             href="#"
-                            className="me-xl-4 opacity_06 ff_way clr_white fw_400 fs_sm"
+                            className="me-lg-4 ff_Montserrat clr_white fw_400 fs_md hover_bold hover_line position-relative"
                             onClick={clickshow}
                           >
                             FAQ
                           </a>
                         </li>
+                        <li>
+                          <div className=" d-flex flex-lg-row flex-column align-items-center d-lg-none">
+                            {/* search */}
+                            <div className="nav_input rounded-pill overflow-hidden pe-3 me-3">
+                              <input
+                                type="text"
+                                placeholder="Search here.."
+                                className="bg_black21 border-0 text-white ff_Montserrat fw_400 fs_xsm"
+                              />
+                              <span>
+                                <img
+                                  src={search}
+                                  alt="search"
+                                  className="pointer"
+                                />
+                              </span>
+                            </div>
+
+                            <button className="ff_way clr_white fw_400 fs_sm sign_btn ms-lg-3 mt-lg-0 mt-4 bg_orange border-0 position-relative button_hover overflow-hidden">
+                              SignUp
+                            </button>
+                          </div>
+                        </li>
                       </ul>
-                      <div className=" d-flex flex-xl-row flex-column">
+                      <div className=" d-lg-flex d-none flex-lg-row flex-column">
                         {/* search */}
-                        <div className="nav_input">
+                        <div className="nav_input rounded-pill overflow-hidden pe-3 me-3">
                           <input
                             type="text"
                             placeholder="Search here.."
-                            className="bg_black21"
+                            className="bg_black21 border-0 text-white ff_Montserrat fw_400 fs_xsm"
                           />
+                          <span>
+                            <img
+                              src={search}
+                              alt="search"
+                              className="pointer"
+                            />
+                          </span>
                         </div>
-                        <span>
+                        <span className="mx-2">
                           <img
                             src={shopping}
                             alt="shopping"
-                            className="w-100"
+                            className="w-100 pointer"
                           />
                         </span>
-                        <button className="ff_way clr_white fw_400 fs_sm sign_btn ms-xl-3 mt-xl-0 mt-4 bg_orange border-0 position-relative button_hover overflow-hidden">
+                        <button className="ff_way clr_white fw_400 fs_sm sign_btn ms-lg-3 mt-lg-0 mt-4 bg_orange border-0 position-relative button_hover overflow-hidden">
                           SignUp
                         </button>
                       </div>
