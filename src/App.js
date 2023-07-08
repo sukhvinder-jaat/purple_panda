2c3dabaa9b08d538c7f191ff820565849cc77963
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-07-08 12:22:55 +0530

    express

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from "./components/MyNav";
 import About from "./components/About";
+import OurRange from "./components/OurRange";
+import ExpressDeliver from "./components/ExpressDeliver";
 function App() {
   return (
     <div className="App bg_backblack">
-      <MyNav />
-      <About />
+      {/* <MyNav />
+      <About /> */}
+      <OurRange />
+      <ExpressDeliver/>
     </div>
   );
 }
