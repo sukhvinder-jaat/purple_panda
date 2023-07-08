779c57278b0fa2cc230cc726d15c5d12b7793ae0
sukhvinder-jaat <monudhaka829@gmail.com>
2023-07-08 14:44:06 +0530

    section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@splidejs/react-splide/css/sea-green";
 import OurRange from "./components/OurRange";
 import { Hero } from "./components/Hero";
 import { Mushrooms } from "./components/Mushrooms";
+import ExpressDelivery from "./components/ExpressDeliver"
+import MyFooter from "./components/MyFooter"
 function App() {
   return (
     <div className="App bg_backblack overflow-hidden">
       <Hero />
       <Mushrooms />
       <OurRange />
-      <ExpressDeliver/>
+      <ExpressDelivery />
+      <MyFooter/>
     </div>
   );
 }
