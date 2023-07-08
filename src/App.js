3d908ecc4474787fb554f9e6d2cd795b1603ccbf
ashok-kumar-032 <av6017819@gmail.com>
2023-07-08 10:24:50 +0530

    btn

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from "./components/MyNav";
 import About from "./components/About";
+import Button from "./components/Button";
 function App() {
   return (
     <div className="App bg_backblack">
       <MyNav />
+      <Button/>
       <About />
     </div>
   );
