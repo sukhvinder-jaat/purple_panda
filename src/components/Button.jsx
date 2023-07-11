ffe6602a4560313d57e860ef7205c0682fdfa19c
sukhvinder-jaat <monudhaka829@gmail.com>
2023-07-11 10:24:47 +0530

    btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = (props) => {
   return (
     <>
-      <button className="fw_700 fs_lg ff_Montserrat btn_all rounded-pill clr_white border-0 lh_normal">
+      <button className="fw_700 fs_lg ff_Montserrat btn_all rounded-pill clr_white border-0 lh_normal all_btn_hover">
         {props.textbtn}
       </button>
     </>
